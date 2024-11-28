9948a0dd2de9337816782fd2d760fe1c2124a892
SanketBhandarii <bhandarisanketp@gmail.com>
2024-11-28 22:33:06 +0530

    First commit

--- src/app.js
+++ src/app.js
@@ -1,13 +1,14 @@
 import express from "express";
 import cors from "cors";
 import router from "./routes/school.routes.js";
+import helmet from "helmet";
 const app = express();
 app.use(
   cors({
     credentials: true,
   })
 );
-
+app.use(helmet());
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use("/api", router);
