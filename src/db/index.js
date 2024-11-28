cf3c25e6a498d1298f0dd33aed3187977d9b6627
SanketBhandarii <bhandarisanketp@gmail.com>
2024-11-28 22:47:37 +0530

    First commit

--- src/db/index.js
+++ src/db/index.js
@@ -1,7 +1,12 @@
+import { config } from "dotenv";
 import mysql from "mysql2";
 
+config({
+  path: "././.env",
+});
+
 const db = mysql.createConnection({
-  host: process.env.DB_HOST || "localhost",
+  host: process.env.DB_HOST || "127.0.0.1",
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "Ganesh",
   database: process.env.DB_NAME || "school_management",
