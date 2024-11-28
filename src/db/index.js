f01b7974d2a195710af767e5bd146a9201d366f2
SanketBhandarii <bhandarisanketp@gmail.com>
2024-11-28 22:52:30 +0530

    MySQL port

--- src/db/index.js
+++ src/db/index.js
@@ -7,6 +7,7 @@ config({
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST || "127.0.0.1",
+  port:3306,
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "Ganesh",
   database: process.env.DB_NAME || "school_management",
